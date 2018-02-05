b6af0d3587a3df87d44d5d6d6505d4d7fde44023
Srikanth Kolli <skolli@berkadia.com>
2018-02-05 19:09:38 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var path = require("path");
+var webpack = require("webpack");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    target: "web",
+    entry: {
+        app: "./src/app.ts"
+    },
+    output: {
+        filename: "src/[name].js",
+        libraryTarget: "amd"
+    },
+    externals: [
+        {
+            "q": true,
+            "react": true,
+            "react-dom": true
+        },
+        /^VSS\/.*/, /^TFS\/.*/, /^q$/
+    ],
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+        moduleExtensions: ["-loader"],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.s?css$/,
+                loaders: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        // new UglifyJSPlugin({
+        //     compress: {
+        //         warnings: false
+        //     },
+        //     output: {
+        //         comments: false
+        //     }
+        // }),
+        new CopyWebpackPlugin([
+            { from: "./node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js", to: "libs/VSS.SDK.min.js" },
+            { from: "./src/index.html", to: "./" },
+            { from: "./src/multi-selection.css", to: "./" },
+            { from: "./img", to: "img" },
+            { from: "./readme.md", to: "readme.md" }
+        ])
+    ]
+}
